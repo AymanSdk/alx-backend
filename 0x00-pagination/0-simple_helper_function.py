fbb209e725c1643156a16235af48ea8186b10569
Aymansdk <ayman.sdk123@outlook.com>
2024-05-09 01:48:08 +0100

    Fixing the helper function tuples

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,14 +1,11 @@
-#!/usr/bin/python3
-"""Helper pagination function
+#!/usr/bin/env python3
+"""Pagination helper function.
 """
-
 from typing import Tuple
 
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
-    """Return a tuple of size two containing a start index and an end index
-    corresponding to the range of indexes to return in a list for those
-    particular pagination parameters.
+    """Retrieves the index range from a given page and page size.
     """
     start = (page - 1) * page_size
     end = start + page_size
